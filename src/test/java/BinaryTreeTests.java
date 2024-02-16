6f3bed60133dbb9dc498999fa5ac90d44ac55d45
Carlos Osmar <carlososmarof@gmail.com>
2024-02-16 15:32:01 -0400

    binary tree tests modified

--- src/test/java/BinaryTreeTests.java
+++ src/test/java/BinaryTreeTests.java
@@ -89,15 +89,7 @@ public class BinaryTreeTests {
         tree.insert(2);
         tree.insert(6);
         tree.insert(10);
-        tree.insert(14);
-        tree.insert(1);
-        tree.insert(3);
-        tree.insert(5);
-        tree.insert(7);
-        tree.insert(9);
-        tree.insert(11);
-        tree.insert(13);
-        tree.insert(15);
+
 
         Assertions.assertEquals(8, tree.getRoot().data);
     }
